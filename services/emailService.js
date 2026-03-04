f822bbaf742ae793ce03d2797a776adbd593c6fe
harmansaini4tech <harmansaini10000@gmail.com>
2026-03-04 23:07:20 +0530

    fix gmail smtp transporter

--- services/emailService.js
+++ services/emailService.js
@@ -2,10 +2,19 @@ const nodemailer = require("nodemailer");
 const logger = require("../utils/logger");
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: +process.env.EMAIL_PORT,
-  secure: false,
-  auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP ERROR:", error);
+  } else {
+    console.log("SMTP Server ready");
+  }
 });
 
 const templates = {
